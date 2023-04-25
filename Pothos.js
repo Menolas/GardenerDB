e4d0f69a95f0b2d0c458c7241761b8a6238c86e7
Elena Christensen <olenachristensen777@gmail.com>
2023-04-25 18:54:29 +0300

    first commit

---
+++ Pothos.js
@@ -0,0 +1,18 @@
+Pothos = {
+    name: 'Pothos',
+    latinName: 'Ficus elastica',
+    nativeArea: ['Nepal', 'Bhutan', 'Burma', 'China', 'Malaysia'],
+    sunExposure: 'Indirect light',
+    soilPH: [5.5, 7.5],
+    toxicity: true,
+    everGreen: true,
+    soilType: {
+        peat: 1,
+        pottingSoil: 1,
+        sand: 1,
+        perlite: 1,
+        pineBark: 0.5,
+        sphagnum: 0
+    },
+    imgURL: 'robusta.webp'
+}
---
+++ Robusta.js
@@ -0,0 +1,18 @@
+Robusta = {
+    name: 'Robusta',
+    latinName: 'Ficus elastica',
+    nativeArea: ['Nepal', 'Bhutan', 'Burma', 'China', 'Malaysia'],
+    sunExposure: 'Indirect light',
+    soilPH: [5.5, 7.5],
+    toxicity: true,
+    everGreen: true,
+    soilType: {
+        peat: 1,
+        pottingSoil: 1,
+        sand: 1,
+        perlite: 1,
+        pineBark: 0.5,
+        sphagnum: 0
+    },
+    imgURL: 'robusta.webp'
+}
